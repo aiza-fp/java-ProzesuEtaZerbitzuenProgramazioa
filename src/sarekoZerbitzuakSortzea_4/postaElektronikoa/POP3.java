72ed8bab4044244eaaa39a924a251bae21231471
aiza-fp <aiza@fpzornotza.com>
2024-12-18 10:27:57 +0100

    sinplifikatu POP3

--- src/sarekoZerbitzuakSortzea_4/postaElektronikoa/POP3.java
+++ src/sarekoZerbitzuakSortzea_4/postaElektronikoa/POP3.java
@@ -4,10 +4,8 @@ import java.util.Properties;
 
 import jakarta.mail.Folder;
 import jakarta.mail.Message;
-import jakarta.mail.Multipart;
 import jakarta.mail.Session;
 import jakarta.mail.Store;
-import jakarta.mail.internet.MimeBodyPart;
 
 /**
  * POP3 (Post Office Protocol 3. bertsioa) protokoloaren erabileraren adibidea.
@@ -18,13 +16,15 @@ import jakarta.mail.internet.MimeBodyPart;
 public class POP3 {
     public static void main(String[] args) {
         String pop3Host = "pop.gmail.com";
-        String username = "zure_email@gmail.com";
-        String password = "zure_pasahitza";
+		String username = "ZURE GMAIL KORREOA";
+		String password = "ZURE KORREOKO APP PASAHITZA";
 
         Properties properties = new Properties();
         properties.put("mail.pop3.host", pop3Host);
         properties.put("mail.pop3.port", "995");
         properties.put("mail.pop3.starttls.enable", "true");
+        properties.put("mail.pop3.rsetbeforequit", "true");
+        properties.put("mail.pop3.leave.on.server", "true");
 
         Session session = Session.getInstance(properties);
         try {
@@ -47,19 +47,7 @@ public class POP3 {
             // Now we can iterate through them normally since we only have the messages we want
             for (Message message : messages) {
                 System.out.println("Mezua: " + message.getSubject());
-                Object content = message.getContent();
-                
-                if (content instanceof String) {
-                    System.out.println("Edukia: " + content);
-                } else if (content instanceof Multipart) {
-                    Multipart multipart = (Multipart) content;
-                    for (int j = 0; j < multipart.getCount(); j++) {
-                        MimeBodyPart part = (MimeBodyPart) multipart.getBodyPart(j);
-                        if (part.isMimeType("text/plain")) {
-                            System.out.println("Edukia: " + part.getContent());
-                        }
-                    }
-                }
+                Object content = message.getContent();                
             }
 
             inbox.close(false);
